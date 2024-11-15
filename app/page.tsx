42b6b388787b23a9c45332dfba5b683bbecb2be6
jokkai2eggs <jokkai2eggs@gmail.com>
2024-11-16 00:06:30 +0700

    set param

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+'use client'
+
 import Image from 'next/image'
 
 export default function Home() {
+  const queryParams = useSearchParams()
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className=" w-[500px] flex flex-col justify-center items-center gap-y-4 p-4 border border-primary">
+      {queryParams}
+      <Image
+        src="/ethay_logo.jpg"
+        alt="logo"
+        width={400}
+        height={0}
+        priority
+        placeholder="empty"
+      />
     </div>
   )
 }
